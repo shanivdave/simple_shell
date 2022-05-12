e6bb39f04d5012d7a36e73bd2fd35c778bf4eee2
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-12 13:47:28 +0300

    handle betty

--- print_char.c
+++ print_char.c
@@ -1,1 +1,28 @@
-#include "holberton.h"
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * _print - prints a string
+ * @str: pointer to the string to print
+ * Return: void
+ */
+void _print(char *str)
+{
+int i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+}
