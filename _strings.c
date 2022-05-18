84f63bdfdff88a8ad9a8b2b9e994c23107a29322
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 12:03:34 +0300

    group work

--- _strings.c
+++ _strings.c
@@ -1,127 +1,102 @@
 #include "main.h"
-
 /**
- * _strcat - concatenate two strings
- * @dest: string to be appended to
- * @src: string to append
- * Return: concatenated string
- */
-char *_strcat(char *dest, char *src)
-{
-	int len = 0;
-	int len2 = 0;
-	int total_len = 0;
-	int j = 0;
-
-	/* find total length of both strings to _realloc */
-	while (dest[len] != '\0')
-	{
-		len++;
-		total_len++;
-	}
-	while (src[len2] != '\0')
-	{
-		len2++;
-		total_len++;
-	}
-
-	/* _realloc because dest was malloced outside of function */
-	dest = _realloc(dest, len, sizeof(char) * total_len + 1);
-
-	while (src[j] != '\0')
-	{
-		dest[len] = src[j];
-		len++;
-		j++;
-	}
-	dest[len] = '\0';
-
-	return (dest);
-}
-/**
- * _strcmp - compare two strings
- * @s1: string 1
- * @s2: string 2
- * Return: int that tells num spaces in between, 0 if exactly the same string
- */
-
-int _strcmp(char *s1, char *s2)
+ * _strcmpdir - compares strings to find dir.
+ *
+ * @s1: string.
+ * @s2: string.
+ *
+ * Return: if match and any other number if otherwise.
+ **/
+int _strcmpdir(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] == s2[i] && s1[i])
-		i++;
-
-	if (s2[i])
-		return (s1[i] - s2[i]);
-	else
-		return (0);
-}
-
-/**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i, len;
-
-	for (len = 0; src[len] != '\0'; len++)
-		;
-
-	for (i = 0; i <= len; i++)
-		dest[i] = src[i];
-
-	return (dest);
-}
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter
- * @str: string to duplicate
- * Return: pointer to duplicated string in allocated memory
- */
-char *_strdup(char *str)
-{
-	char *duplicate_str;
-	int i, len = 0;
-
-	if (str == NULL) /* validate str input */
-		return (NULL);
-
-	while (str[len])
-		len++;
-	len++; /* add null terminator to length */
-
-	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
-	if (duplicate_str == NULL)
-		return (NULL);
-
-	i = 0;
-	while (i < len)
+	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
 	{
-		duplicate_str[i] = str[i];
+		if (i == 3)
+			break;
 		i++;
+		s2++;
 	}
 
-	return (duplicate_str);
+	return (*s1 - *s2);
+}
+/**
+ * charput - writes the character like putchar
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int charput(char c)
+{
+	return (write(1, &c, 1));
 }
 
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string
- * Return: characters in the string
+ * place - similar to puts in C
+ * @str: a pointer the integer we want to set to 402
+ *
+ * Return: int
  */
-int _strlen(const char *s)
+void place(char *str)
+{
+	while (*str != '\0')
+	{
+		charput(*str);
+		str++;
+	}
+}
+
+/**
+ * _strlen - Len string.
+ * @str: My string.
+ * Return: Length.
+ */
+int _strlen(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		;
+
 	return (i);
+}
+
+/**
+ * str_concat - concatane strings.
+ * @s1: string.
+ * @s2: second string.
+ * Return: strings.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	int lens1, lens2, j, i, e;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	lens1 = _strlen(s1);
+
+	lens2 = _strlen(s2);
+
+	a = malloc(((lens1) + (lens2) + 1) * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (j = 0; j < lens1; j++)
+	{
+		a[j] = s1[j];
+	}
+
+	for (i = lens1, e = 0; e <= lens2; i++, e++)
+	{
+		a[i] = s2[e];
+	}
+	return (a);
 }
