a166a5122aab4735795a106683d70ac92e05320f
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 21:15:52 +0300

    update repo

--- builtins.c
+++ builtins.c
@@ -1,9 +1,3 @@
-/*
- * File: builtin.c
- * Auth: Alex Yu
- *       Brennan D Baraban
- */
-
 #include "shell.h"
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
