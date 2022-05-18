dddb5a25a480a16248c23f5b8884a2ed005527c3
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 11:58:00 +0300

    group work

--- execute.c
+++ execute.c
@@ -1,113 +1,49 @@
 #include "main.h"
 
 /**
- * _occurence - return the number of occurence of a string
- * @s: string to check
- * Return: integer
+ * execute_proc - similar to puts in C
+ * @cmd: a pointer the integer we want to set to 402
+ *
+ * Return: int
  */
-unsigned int _occurence(char *s)
+void execute_proc(char **cmd)
 {
-	int i, cnt = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	char *parametro = (*(cmd + 1));
+	char *s, *slash = "/";
+	char *o;
+
+	char *vartoprint = *cmd;
+	char *argv[4];
+
+	if ((access(cmd[0], F_OK) == 0))
 	{
-		/*test for all delimeters*/
-		if (s[i]  == ' ' || s[i] == '\t' || s[i] == '\n')
-			cnt++;
-	}
+		argv[0] = cmd[0];
+		argv[1] = parametro;
+		argv[2] = ".";
+		argv[3] = NULL;
 
-	return (cnt);
-}
-/**
- *_strtotokens - split a sentence into multiple words.
- *@str: the string passed as argument.
- *Return: tokens
- */
-char **_strtotokens(char *str)
-{
-	int i = 0;
-	const char separator[] = " \t\n";
-	int spaces = _occurence(str);
-	char **tokens = malloc(sizeof(char *) * (spaces + 1));
-	char *token;
-
-	if (!tokens)
-	{
-		fprintf(stderr, "sh: allocation error\n");
-		exit(1);
-	}
-
-	token = strtok(str, separator);
-
-	while (token != NULL)
-	{
-		tokens[i] = token;
-		token = strtok(NULL, separator);
-		i++;
-	}
-	tokens[i] = NULL;
-	return (tokens);
-}
-/**
- *check_file_status - checks whether a file exists
- *@filename:name of file
- *Return:0 - file is available, -1 otherwise
- */
-int check_file_status(char *filename)
-{
-	struct stat st;
-
-	if (stat(filename, &st) == -1)
-		return (-1);
-	return (0);
-}
-/**
- *_execute - executes a file given as input
- *@tokens:split tokens from stdin input
- *@line:line from stdin to free
- *Return:-1 (success), -1,otherwise
- */
-int _execute(char **tokens, char *line, char *args)
-{
-	char *err1, *err2, *err3;
-	pid_t cpid;
-	int status;
-	struct stat st;
-
-	/*handle when token is a builtin cmd or NULL*/
-	if (builtin_parser(tokens) == 0 || *tokens == NULL)
-	{
-		return (1);
-	}
-	/*fork the process*/
-	cpid = fork();
-	if (cpid < 0)
-	{
-		perror("Error:fork->-1");
-		return (EXIT_FAILURE);
-	}
-/*child process*/
-	if (cpid == 0)
-	{
-		if (stat(*tokens, &st) != 0)
+		if (execve(argv[0], argv, NULL) == -1)
 		{
-			get_path(tokens);
+			perror("Error");
 		}
-		if (execve(tokens[0], tokens, NULL) == -1)
-		{
-			err1 = strcat(*tokens, ": No such file or directory\n");
-			err2 = strcat(args, ":");
-			err3 = strcat(err2, err1);
-			write(STDERR_FILENO, err3, _strlen(err3));
-			/*free(err1);*/
-			/*free(err2);*/
-			/*free(err3);*/
-			free(line);
-			free(tokens);
-			exit(EXIT_FAILURE);
-		}
-		return (EXIT_SUCCESS);
 	}
-wait(&status);
-return (1);
+	else
+	{
+		o = find_command(vartoprint);
+
+		slash = str_concat(o, slash);
+
+		s = str_concat(slash, *cmd);
+
+		argv[0] = s;
+		argv[1] = parametro;
+		argv[2] = ".";
+		argv[3] = NULL;
+
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
 }
