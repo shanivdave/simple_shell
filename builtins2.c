3d4253f67c1ba5019b3fc7ad2c58466b5e881be4
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 21:08:54 +0300

    update repo

--- builtins2.c
+++ builtins2.c
@@ -1,118 +1,158 @@
+/*
+ * File: builtin.c
+ * Auth: Alex Yu
+ *       Brennan D Baraban
+ */
+
 #include "shell.h"
 
-/**
- * add_key - create a new environment variable
- * @vars: pointer to struct of variables
- *
- * Return: void
- */
-void add_key(vars_t *vars)
-{
-  unsigned int i;
-  char **newenv;
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias(char *var_name, char *value);
+void print_alias(alias_t *alias);
 
-  for (i = 0; vars->env[i] != NULL; i++)
-    ;
-  newenv = malloc(sizeof(char *) * (i + 2));
-  if (newenv == NULL)
-    {
-      print_error(vars, NULL);
-      vars->status = 127;
-      new_exit(vars);
-    }
-  for (i = 0; vars->env[i] != NULL; i++)
-    newenv[i] = vars->env[i];
-  newenv[i] = add_value(vars->av[1], vars->av[2]);
-  if (newenv[i] == NULL)
-    {
-      print_error(vars, NULL);
-      free(vars->buffer);
-      free(vars->commands);
-      free(vars->av);
-      free_env(vars->env);
-      free(newenv);
-      exit(127);
-    }
-  newenv[i + 1] = NULL;
-  free(vars->env);
-  vars->env = newenv;
+/**
+ * shellby_alias - Builtin command that either prints all aliases, specific
+ * aliases, or sets an alias.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 0.
+ */
+int shellby_alias(char **args, char __attribute__((__unused__)) **front)
+{
+	alias_t *temp = aliases;
+	int i, ret = 0;
+	char *value;
+
+	if (!args[0])
+	{
+		while (temp)
+		{
+			print_alias(temp);
+			temp = temp->next;
+		}
+		return (ret);
+	}
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		value = _strchr(args[i], '=');
+		if (!value)
+		{
+			while (temp)
+			{
+				if (_strcmp(args[i], temp->name) == 0)
+				{
+					print_alias(temp);
+					break;
+				}
+				temp = temp->next;
+			}
+			if (!temp)
+				ret = create_error(args + i, 1);
+		}
+		else
+			set_alias(args[i], value);
+	}
+	return (ret);
 }
 
 /**
- * find_key - finds an environment variable
- * @env: array of environment variables
- * @key: environment variable to find
- *
- * Return: pointer to address of the environment variable
+ * set_alias - Will either set an existing alias 'name' with a new value,
+ * 'value' or creates a new alias with 'name' and 'value'.
+ * @var_name: Name of the alias.
+ * @value: Value of the alias. First character is a '='.
  */
-char **find_key(char **env, char *key)
+void set_alias(char *var_name, char *value)
 {
-  unsigned int i, j, len;
+	alias_t *temp = aliases;
+	int len, j, k;
+	char *new_value;
 
-  len = _strlen(key);
-  for (i = 0; env[i] != NULL; i++)
-    {
-      for (j = 0; j < len; j++)
-	if (key[j] != env[i][j])
-	  break;
-      if (j == len && env[i][j] == '=')
-	return (&env[i]);
-    }
-  return (NULL);
+	*value = '\0';
+	value++;
+	len = _strlen(value) - _strspn(value, "'\"");
+	new_value = malloc(sizeof(char) * (len + 1));
+	if (!new_value)
+		return;
+	for (j = 0, k = 0; value[j]; j++)
+	{
+		if (value[j] != '\'' && value[j] != '"')
+			new_value[k++] = value[j];
+	}
+	new_value[k] = '\0';
+	while (temp)
+	{
+		if (_strcmp(var_name, temp->name) == 0)
+		{
+			free(temp->value);
+			temp->value = new_value;
+			break;
+		}
+		temp = temp->next;
+	}
+	if (!temp)
+		add_alias_end(&aliases, var_name, new_value);
 }
 
 /**
- * add_value - create a new environment variable string
- * @key: variable name
- * @value: variable value
- *
- * Return: pointer to the new string;
+ * print_alias - Prints the alias in the format name='value'.
+ * @alias: Pointer to an alias.
  */
-char *add_value(char *key, char *value)
+void print_alias(alias_t *alias)
 {
-  unsigned int len1, len2, i, j;
-  char *new;
+	char *alias_string;
+	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
 
-  len1 = _strlen(key);
-  len2 = _strlen(value);
-  new = malloc(sizeof(char) * (len1 + len2 + 2));
-  if (new == NULL)
-    return (NULL);
-  for (i = 0; key[i] != '\0'; i++)
-    new[i] = key[i];
-  new[i] = '=';
-  for (j = 0; value[j] != '\0'; j++)
-    new[i + 1 + j] = value[j];
-  new[i + 1 + j] = '\0';
-  return (new);
+	alias_string = malloc(sizeof(char) * (len + 1));
+	if (!alias_string)
+		return;
+	_strcpy(alias_string, alias->name);
+	_strcat(alias_string, "='");
+	_strcat(alias_string, alias->value);
+	_strcat(alias_string, "'\n");
+
+	write(STDOUT_FILENO, alias_string, len);
+	free(alias_string);
 }
-
 /**
- * _atoi - converts a string into an integer
- * @str: string to convert
+ * replace_aliases - Goes through the arguments and replace any matching alias
+ * with their value.
+ * @args: 2D pointer to the arguments.
  *
- * Return: the integer value, or -1 if an error occurs
+ * Return: 2D pointer to the arguments.
  */
-int _atoi(char *str)
+char **replace_aliases(char **args)
 {
-  unsigned int i, digits;
-  int num = 0, num_test;
+	alias_t *temp;
+	int i;
+	char *new_value;
 
-  num_test = INT_MAX;
-  for (digits = 0; num_test != 0; digits++)
-    num_test /= 10;
-  for (i = 0; str[i] != '\0' && i < digits; i++)
-    {
-      num *= 10;
-      if (str[i] < '0' || str[i] > '9')
-	return (-1);
-      if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
-	return (-1);
-      num += str[i] - '0';
-      if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
-	return (-1);
-    }
-  if (i > digits)
-    return (-1);
-  return (num);
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		while (temp)
+		{
+			if (_strcmp(args[i], temp->name) == 0)
+			{
+				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
+				if (!new_value)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_value, temp->value);
+				free(args[i]);
+				args[i] = new_value;
+				i--;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+
+	return (args);
 }
