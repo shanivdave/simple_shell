c7b963b42c999b275415a533a99838460e6c28b1
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 12:28:15 +0300

    Update engine.c

--- engine.c
+++ engine.c
@@ -1,99 +1,28 @@
 #include "main.h"
 
 /**
- * lookforslash - identifies if first char is a slash.
- * @cmd: first string
- * Return: 1 if yes 0 if no.
+ * Run - Check if is built-in
+ *
+ * @Arg_str: argument string.
+ * @ct_output: output.
+ * @row: row arguments.
+ *
+ * Return: returns other than exit or env
  */
-int lookforslash(char *cmd)
+
+int Run(char **Arg_str, int ct_output, char *row)
 {
-	int cont = 0;
+	char *built_box[2] = {"exit", "env"};
 
-	while (cmd[cont])
-	{
-		if (cmd[0] == '/')
-		{
-			printf("%c\n", cmd[0]);
-			return (1);
-		}
+	if (*Arg_str == NULL)
+		return (1);
 
-		cont++;
-	}
-	return (0);
-}
+	else if (_strcmp(built_box[0], Arg_str[0]) == 0)
+		return (built_in(Arg_str, ct_output, row));
 
-/**
- * compareExit - identifies if first char is a slash.
- * @s1: first string
- * @s2: exit string
- * Return: 1 if yes 0 if no.
- */
-int compareExit(char *s1, char *s2)
-{
-	int i = 0;
+	else if (_strcmp(built_box[1], Arg_str[0]) == 0)
+		return (built_in(Arg_str, ct_output, row));
 
-	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-	{
-		if (i == 3)
-			break;
-		i++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
-}
-
-/**
- * compareEnv - identifies if first char is a slash.
- * @s1: first string
- * @s2: exit string
- * Return: 1 if yes 0 if no.
- */
-int compareEnv(char *s1, char *s2)
-{
-	int i = 0;
-
-	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-	{
-		if (i == 2)
-			break;
-		i++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
-}
-/**
- * identify_string - identyfy keyboard input.
- * @parameter: call prompt from another function (prompt)
- * Return: str
- **/
-char **identify_string(char *parameter)
-{
-	char **buf = malloc(1024 * sizeof(char *));
-	char *split;
-	int i = 0;
-	char *delim = " \t\n";
-
-
-	split = strtok(parameter, delim);
-
-	while (split != NULL)
-	{
-		buf[i] = split;
-		i++;
-		split = strtok(NULL, delim);
-	}
-	execute_proc(buf);
-	return (buf);
-
-}
-/**
- * controlC - avoid close the shell
- * @sig: keep going shell
- **/
-void  controlC(int sig)
-{
-	(void) sig;
-	write(1, "\n$ ", 3);
+	else
+		return (comp_Arg(Arg_str, ct_output));
 }
