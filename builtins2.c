53030e9fd4fa42d328a3b0b24492ef179489b750
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 20:58:50 +0300

    update repo

--- builtins2.c
+++ builtins2.c
@@ -73,18 +73,18 @@ char *add_value(char *key, char *value)
   unsigned int len1, len2, i, j;
   char *new;
 
-    len1 = _strlen(key);
-    len2 = _strlen(value);
-    new = malloc(sizeof(char) * (len1 + len2 + 2));
-    if (new == NULL)
+  len1 = _strlen(key);
+  len2 = _strlen(value);
+  new = malloc(sizeof(char) * (len1 + len2 + 2));
+  if (new == NULL)
     return (NULL);
-    for (i = 0; key[i] != '\0'; i++)
+  for (i = 0; key[i] != '\0'; i++)
     new[i] = key[i];
-    new[i] = '=';
-    for (j = 0; value[j] != '\0'; j++)
+  new[i] = '=';
+  for (j = 0; value[j] != '\0'; j++)
     new[i + 1 + j] = value[j];
-    new[i + 1 + j] = '\0';
-    return (new);
+  new[i + 1 + j] = '\0';
+  return (new);
 }
 
 /**
