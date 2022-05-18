d6a6517eab421b39f468a7997022f7bb5a7e9159
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 12:20:07 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,17 +1,29 @@
 #include "main.h"
 
 /**
- * main - func with infinite loop
- * @ac: No use
- * @av: No use
- * Return: loop.
- **/
-int main(int ac, char **av)
-{
-	(void)av;
-	(void)ac;
+ * main - start shell.
+ *
+ * Return: (0).
+ */
 
-	signal(SIGINT, controlC);
-	prompt();
+int main(void)
+{
+	char *row = NULL;
+	char **tokens = NULL;
+	int ct_output = 0;
+
+	while (1)
+	{
+		if (isatty(STDIN))
+			write(STDOUT, "#holbies$ ", 10);
+
+		row = rd_row();
+		tokens = tokenizer(row);
+		ct_output = Run(tokens, ct_output, row);
+
+		free(row);
+		free(tokens);
+	}
+
 	return (0);
 }
