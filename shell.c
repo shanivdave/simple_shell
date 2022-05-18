d084e3aa3888366d044639f5c22c2728c8370f07
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 13:41:28 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,29 +1,50 @@
-#include "main.h"
-
+#include "shell.h"
 /**
- * main - start shell.
- *
- * Return: (0).
- */
+* main - carries out the read, execute then print output loop
+* @ac: argument count
+* @av: argument vector
+* @envp: environment vector
+*
+* Return: 0
+*/
 
-int main(void)
+int main(int ac, char **av, char *envp[])
 {
-	char *row = NULL;
-	char **tokens = NULL;
-	int ct_output = 0;
-
+	char *line = NULL, *pathcommand = NULL, *path = NULL;
+	size_t bufsize = 0;
+	ssize_t linesize = 0;
+	char **command = NULL, **paths = NULL;
+	(void)envp, (void)av;
+	if (ac < 1)
+		return (-1);
+	signal(SIGINT, handle_signal);
 	while (1)
 	{
-		if (isatty(STDIN))
-			write(STDOUT, "#holbies$ ", 10);
-
-		row = rd_row();
-		tokens = tokenizer(row);
-		ct_output = Run(tokens, ct_output, row);
-
-		free(row);
-		free(tokens);
+		free_buffers(command);
+		free_buffers(paths);
+		free(pathcommand);
+		prompt_user();
+		linesize = getline(&line, &bufsize, stdin);
+		if (linesize < 0)
+			break;
+		info.ln_count++;
+		if (line[linesize - 1] == '\n')
+			line[linesize - 1] = '\0';
+		command = tokenizer(line);
+		if (command == NULL || *command == NULL || **command == '\0')
+			continue;
+		if (checker(command, line))
+			continue;
+		path = find_path();
+		paths = tokenizer(path);
+		pathcommand = test_path(paths, command[0]);
+		if (!pathcommand)
+			perror(av[0]);
+		else
+			execution(pathcommand, command);
 	}
-
+	if (linesize < 0 && flags.interactive)
+		write(STDERR_FILENO, "\n", 1);
+	free(line);
 	return (0);
 }
