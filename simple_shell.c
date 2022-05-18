6b84c478c7a3be65805ad14714e83a39a37ecbc3
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-05-18 20:48:27 +0300

    update repo

--- simple_shell.c
+++ simple_shell.c
@@ -1,7 +1,5 @@
 #include "shell.h"
 
-/* global variable for ^C handling */
-unsigned int sig_flag;
 
 /**
  * sig_handler - handles ^C signal interupt
@@ -11,6 +9,7 @@ unsigned int sig_flag;
  */
 static void sig_handler(int uuv)
 {
+	unsigned int sig_flag;
 	(void) uuv;
 
 	if (sig_flag == 0)
